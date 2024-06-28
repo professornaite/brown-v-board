157be03114cf87d79479023eeee0e089b72f3195
professornaite <professornaite@gmail.com>
2024-06-28 12:57:23 -0400

    pre processing

--- code/py/test.py
+++ code/py/test.py
@@ -1,3 +1,6 @@
+
+# https://lab.constellate.org
+
 ### Import modules and libraries ###
 
 import constellate
@@ -11,5 +14,16 @@ import gzip
 %matplotlib inline
 
 # Creating a variable `dataset_id` to hold our dataset ID
-# The default dataset is Shakespeare Quarterly, 1950-present
-dataset_id = "92c32bef-5e54-8741-1e64-a417053d18cd"
+# dataset_id = ""
+
+# Pull in the sampled (1500 items) dataset CSV using
+# The .get_metadata() method downloads the CSV file for our metadata
+# to the /data folder and returns a string for the file name and location
+# dataset_metadata will be a string containing that file name and location
+dataset_metadata = constellate.get_metadata("data/bvb-jsonl.jsonl")
+
+# Download the full dataset (up to a limit of 25,000 documents),
+# request it first in the builder environment. See the Constellate Client
+# documentation at: https://constellate.org/docs/constellate-client
+# Then use the `constellate.download` method show below.
+#dataset_metadata = constellate.download(dataset_id, 'metadata')
